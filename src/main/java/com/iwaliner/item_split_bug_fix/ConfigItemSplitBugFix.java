68646500dd2d30d008535d5c26555c05b920c716
luoxueyasha <bing1273103582@outlook.com>
2025-05-25 12:09:54 +0800

    fix config

--- src/main/java/com/iwaliner/item_split_bug_fix/ConfigItemSplitBugFix.java
+++ src/main/java/com/iwaliner/item_split_bug_fix/ConfigItemSplitBugFix.java
@@ -3,6 +3,7 @@ package com.iwaliner.item_split_bug_fix;
 import org.apache.commons.lang3.tuple.Pair;
 import net.minecraftforge.common.ForgeConfigSpec;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class ConfigItemSplitBugFix {
@@ -23,23 +24,25 @@ public class ConfigItemSplitBugFix {
     private ConfigItemSplitBugFix(ForgeConfigSpec.Builder builder) {
         builder.comment("Config file for ItemSplitBugFix");
 
-        builder.push("Blacklist Settings");
+        builder.push("blacklist_settings");
 
         BLACKLIST_ITEMS = builder
-            .comment("Items in this list will not be modified by ItemSplitBugFix.",
-                "Supports wildcard '*'. For example:",
-                "- 'minecraft:diamond' matches exactly 'minecraft:diamond'",
-                "- '*:diamond*' matches any item from any mod that contains 'diamond'",
-                "- 'minecraft:*' matches all items from minecraft namespace",
-                "- '*nec*' matches any item that contains 'nec', eg. 'necromancy:abc', 'my_mod:necklace'")
+            .comment(" Items in this list will not be modified by ItemSplitBugFix.",
+                " Supports wildcard '*'. For example:",
+                "   - 'minecraft:diamond' matches exactly 'minecraft:diamond'",
+                "   - '*:diamond*' matches any item from any mod that contains 'diamond'",
+                "   - 'minecraft:*' matches all items from minecraft namespace",
+                "   - '*nec*' matches any item that contains 'nec', eg. 'necromancy:abc', 'my_mod:necklace'")
             // .translation("config.itemsplitbugfix.blacklist_items")   // @debug: no need for translation files
-            .define("Blacklist Items", List.of(
-                "alexscaves:sack_of_sating",
-                "hexerei:*",
-                "spelunkery:*",
-                "minecraft:spyglass",
-                "minecraft:air"
-            ));
+            .defineList("blacklist", Arrays.asList(
+                    "alexscaves:sack_of_sating",
+                    "hexerei:*",
+                    "spelunkery:*",
+                    "minecraft:spyglass",
+                    "technicalcores:*"
+                ),
+                entry -> true
+            );
 
         builder.pop();
 
